f050a41931e5761da9399c87f004b2a75407423d
Anton Patron <patronant@gmail.com>
2018-11-20 23:18:51 +0100

    Big commit

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
 'use strict';
 const express = require('express');
-const http = require('http');
+const path = require('path');
+const debug = require('debug');
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes/index');
+const server = require('http').createServer(app);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+
+    app.set('port', process.env.PORT || 3000);
+    server.listen(app.get('port'), function () {
+        debug('Express server listening on port ' + server.address().port);
+    });
+
+}
