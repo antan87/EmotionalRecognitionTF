0934dcbf353574a89075f4d4802621ce8f429e9a
Anton Patron <patronant@gmail.com>
2018-11-24 14:30:09 +0100

    Test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let video = null;
 let canvas = null;
 let ctx = null;
@@ -29,9 +30,42 @@ function setup() {
       });
   };
   ctx = canvas.getContext("2d");
+=======
+  // TODO: put you code here 
+  'use strict';
+  let canvas = null;
+  let ctx = null;
+  let btnSnap = null;
+  let faceShapes = null;
+  const faceDetector = new FaceDetector();
+  const socket = io.connect('http://localhost:3000');
+  let img = null;
+  let imgP5 = null;
+
+  socket.on('imageConversionByServer', function (data) {
+
+    img = new Image();
+    img.setAttribute('src', data);
+    imgP5 = loadImage(data);
+    console.log(img);
+    resizeCanvas(img.width, img.height);
+  });
+
+
+  function setup() {
+    btnSnap = document.getElementById('btn-snap');
+    btnSnap.addEventListener("click", function () {
+      takeSnapShot();
+    });
+    createCanvas(1400, 1400);
+  }
+
+  function draw() {
+>>>>>>> Stashed changes
 
   btnSnap = document.getElementById('btn-snap');
 
+<<<<<<< Updated upstream
   btnSnap.addEventListener("click", function () {
     takeSnapShot();
   });
@@ -61,9 +95,26 @@ function draw() {
 
     // A rectangle
     rect(left, top, width, height);
+=======
+    if (faceShapes !== null) {
+      faceShapes.forEach((faceShape) => {
+        const {
+          width,
+          height,
+          top,
+          left
+        } = faceShape.boundingBox;
+
+        stroke(0);
+        noFill();
+        rect(left, top, width, height);
+      });
+    }
+>>>>>>> Stashed changes
   }
 }
 
+<<<<<<< Updated upstream
 async function takeSnapShot() {
   if (video === null || canvas === null)
     return;
@@ -167,3 +218,30 @@ function drawFace(img) {
 //   }
 
 //  })
+=======
+  async function takeSnapShot() {
+
+    const scale = img.width / img.naturalWidth;
+    faceDetector
+      .detect(img)
+      .then((faces) => {
+        faceShapes = faces;
+        console.log(faces);
+        return faces;
+      })
+      .catch(err => console.log(err));
+
+  }
+
+  const drawFaceBox = (height, width, top, left, scale) => {
+    const div = document.createElement('div');
+    div.className = 'face-box';
+    div.style.cssText = `
+      top: ${top * scale}px;
+      left: ${left * scale}px;
+      height: ${height * scale}px;
+      width: ${width * scale}px;
+    `;
+    return div;
+  };
+>>>>>>> Stashed changes
--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,21 @@
         });
     });
 
+<<<<<<< Updated upstream
+=======
+    io.on('connection', (socket) => {
+        // fs.readFile('./images/kim-un.jpg', function (err, data) {
+        fs.readFile('./images/img4.jpg', function (err, data) {
+            socket.emit('imageConversionByClient', {
+                image: true,
+                buffer: data
+            });
+            socket.emit('imageConversionByServer', "data:image/png;base64," + data.toString("base64"));
+        });
+
+    });
+
+    return router;
+>>>>>>> Stashed changes
 
 module.exports = router;
