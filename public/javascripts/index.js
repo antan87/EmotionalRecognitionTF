d62900cc5e59b987d733da153b4b51d1b72bede9
Anton Patron <patronant@gmail.com>
2018-11-24 13:26:20 +0100

    Fixed face recgonition writed box

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,88 +1,69 @@
   // TODO: put you code here 
 
-let canvas = null;
-let ctx = null;
-let btnSnap = null;
-let face = null;
-const faceDetector = new FaceDetector();
-console.log(faceDetector);
-const socket = io.connect('http://localhost:3000');
+  let canvas = null;
+  let ctx = null;
+  let btnSnap = null;
+  let faceShape = null;
+  const faceDetector = new FaceDetector();
+  const socket = io.connect('http://localhost:3000');
+  let img = null;
+  let imgP5 = null;
 
-socket.on('imageConversionByServer', function(data) {
-  
-  let img = document.getElementById('img-container');
-  img.setAttribute('src', data);
-  
-});
+  socket.on('imageConversionByServer', function (data) {
 
-
-function setup() {
-  canvas = document.createElement('canvas');
-
-  canvas.id = "CursorLayer";
-  canvas.width = 500;
-  canvas.height = 375;
-  canvas.style.position = "absolute";
-  canvas.style.border = "1px solid";
-
- 
-  ctx = canvas.getContext("2d");
-  btnSnap = document.getElementById('btn-snap');
-
-  btnSnap.addEventListener("click", function () {
-    takeSnapShot();
+    img = new Image();
+    img.setAttribute('src', data);
+    imgP5 = loadImage(data);
   });
 
-}
 
-function draw() {
-  let img = document.getElementById('img-container');
-    background(0);
-  // image(capture, 0, 0, 320, 240);
-  // filter('INVERT');
-
-  if (face !== null) {
-
-    const {
-      width,
-      height,
-      top,
-      left
-    } = face.boundingBox;
-
-    fill(204, 101, 192, 127);
-    stroke(127, 63, 120);
-
-    // A rectangle
-    rect(left, top, width, height);
+  function setup() {
+    btnSnap = document.getElementById('btn-snap');
+    btnSnap.addEventListener("click", function () {
+      takeSnapShot();
+    });
+    createCanvas(720, 400);
   }
-}
 
-async function takeSnapShot() {
+  function draw() {
 
-  let img = document.getElementById('img-container');
-  const scale = img.width / img.naturalWidth;
-  faceDetector
-    .detect(img)
-    .then(faces => 
-      faces.map((face) => {
-        console.log(face);
-        return face.boundingBox
-      })
-    )
-    .then(faceBoxes => {
-      faceBoxes.forEach(faceBox => {
-        const { 
-          height, width, top, left 
-        } = faceBox;
+    if (imgP5 !== null) {
+      image(imgP5, 0, 0);
+    }
 
-        const div = drawFaceBox(
-          height, width, top, left, scale
-        );
-        img.parentElement.appendChild(div);
-      });
-    })
-    .catch(err => console.log(err));
+    if (faceShape !== null) {
+
+      const {
+        width,
+        height,
+        top,
+        left
+      } = faceShape.boundingBox;
+
+      stroke(0);
+      noFill();
+      rect(left, top, width, height);
+
+      // Setting the outline (stroke) to black
+      // A rectangle
+
+
+
+    }
+  }
+
+  async function takeSnapShot() {
+
+    const scale = img.width / img.naturalWidth;
+    faceDetector
+      .detect(img)
+      .then(faces =>
+        faces.map((face) => {
+          faceShape = face;
+          return face.boundingBox
+        })
+      )
+      .catch(err => console.log(err));
 
   }
 
@@ -97,87 +78,68 @@ async function takeSnapShot() {
     `;
     return div;
   };
-  
 
 
-function drawFace(img) {
-  if (face === null)
-    return;
 
 
-  const {
-    width,
-    height,
-    top,
-    left
-  } = face.boundingBox;
+
+  // })
+
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   const video = document.getElementById('video-container');
+  //   const canvas = document.getElementById('img-container');
+  //   const faceDetector = new window.FaceDetector()
+  //   let faces = null;
+  //   let context = canvas.getContext('2d')
+  //   const videoCompStyle = window.getComputedStyle(video)
+  //   const videoWidth = videoCompStyle.width.replace('px', '')
+  //   const videoHeight = videoCompStyle.height.replace('px', '')
+
+  //   setTimeout(100, tt());
 
 
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
-
-  // A rectangle
-  rect(width, 500, 600, height);
-}
-
-
-// })
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const video = document.getElementById('video-container');
-//   const canvas = document.getElementById('img-container');
-//   const faceDetector = new window.FaceDetector()
-//   let faces = null;
-//   let context = canvas.getContext('2d')
-//   const videoCompStyle = window.getComputedStyle(video)
-//   const videoWidth = videoCompStyle.width.replace('px', '')
-//   const videoHeight = videoCompStyle.height.replace('px', '')
-
-//   setTimeout(100, tt());
+  //   async function tt() {
+  //     context = canvas.getContext('2d')
+  //     context.drawImage(video, 0, 0, videoWidth, videoHeight)
+  //     faces = await faceDetector.detect(canvas);
+  //     console.log(faces);
+  //   };
 
 
-//   async function tt() {
-//     context = canvas.getContext('2d')
-//     context.drawImage(video, 0, 0, videoWidth, videoHeight)
-//     faces = await faceDetector.detect(canvas);
-//     console.log(faces);
-//   };
+  //   if (navigator.mediaDevices.getUserMedia) {
+  //     navigator.mediaDevices.getUserMedia({
+  //         video: true
+  //       })
+  //       .then(function (stream) {
+  //         video.srcObject = stream;
+  //       })
+  //       .catch(function (err0r) {
+  //         console.log("Something went wrong!");
+  //       });
+  //   };
+  //   // loop(canvas, video, 100);
+  //   let hideTimeout = 100;
+  //   async function loop(canvas, video, overlay) {
+  //     window.requestAnimationFrame(() => loop(canvas, video, overlay))
+  //     const context = canvas.getContext('2d')
+  //     const videoCompStyle = window.getComputedStyle(video)
+  //     const videoWidth = videoCompStyle.width.replace('px', '')
+  //     const videoHeight = videoCompStyle.height.replace('px', '')
+  //     context.drawImage(video, 0, 0, videoWidth, videoHeight)
 
+  //     clearTimeout(hideTimeout)
+  //     if (faces !== null && faces.length) {
+  //       const face = faces[0].boundingBox
+  //       console.log('Face detected on video frame: ', face)
+  //     } else {
+  //       // hideTimeout = hideOverlay(overlay)
+  //     }
 
-//   if (navigator.mediaDevices.getUserMedia) {
-//     navigator.mediaDevices.getUserMedia({
-//         video: true
-//       })
-//       .then(function (stream) {
-//         video.srcObject = stream;
-//       })
-//       .catch(function (err0r) {
-//         console.log("Something went wrong!");
-//       });
-//   };
-//   // loop(canvas, video, 100);
-//   let hideTimeout = 100;
-//   async function loop(canvas, video, overlay) {
-//     window.requestAnimationFrame(() => loop(canvas, video, overlay))
-//     const context = canvas.getContext('2d')
-//     const videoCompStyle = window.getComputedStyle(video)
-//     const videoWidth = videoCompStyle.width.replace('px', '')
-//     const videoHeight = videoCompStyle.height.replace('px', '')
-//     context.drawImage(video, 0, 0, videoWidth, videoHeight)
+  //     if (isDetectingFaces) return
 
-//     clearTimeout(hideTimeout)
-//     if (faces !== null && faces.length) {
-//       const face = faces[0].boundingBox
-//       console.log('Face detected on video frame: ', face)
-//     } else {
-//       // hideTimeout = hideOverlay(overlay)
-//     }
+  //     isDetectingFaces = true
+  //     faces = await faceDetector.detect(canvas)
+  //     isDetectingFaces = false
+  //   }
 
-//     if (isDetectingFaces) return
-
-//     isDetectingFaces = true
-//     faces = await faceDetector.detect(canvas)
-//     isDetectingFaces = false
-//   }
-
-//  })
+  //  })
--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,23 @@ module.exports = (io) => {
 
     /* GET home page. */
     router.get('/', function (req, res) {
-    res.render('index', {
+        res.render('index', {
             title: 'Express'
         });
     });
 
     io.on('connection', (socket) => {
-        fs.readFile('./images/kim-un.jpg', function(err, data){
-            socket.emit('imageConversionByClient', { image: true, buffer: data });
-            socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"));
-          });
+        // fs.readFile('./images/kim-un.jpg', function (err, data) {
+        fs.readFile('./images/kim-un.jpg', function (err, data) {
+            socket.emit('imageConversionByClient', {
+                image: true,
+                buffer: data
+            });
+            socket.emit('imageConversionByServer', "data:image/png;base64," + data.toString("base64"));
+        });
 
     });
 
-return router;
+    return router;
 
 };
